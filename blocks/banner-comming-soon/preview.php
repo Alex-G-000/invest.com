fe403c0f1fa4e67c04b73d805249534db788639d
Alexey-Geyvan <alexey.geyvan@kawalee.com>
2022-03-04 10:32:08 +0200

    preview fix

--- blocks/banner-comming-soon/preview.php
+++ blocks/banner-comming-soon/preview.php
@@ -1,10 +1,3 @@
 <div class="block-preview">
-    <?php 
-    if ( get_field('homepage-banner-soon_banner-preview', 'options') ) { ?>
-        <img src="<?php echo wp_get_attachment_image_url( get_field('homepage-banner-soon_banner-preview', 'options'), 'full' ); ?>" alt="" class="preview-img"> 
-    <?php
-    } else {
-        echo '<h2>Banner comming soon</h2>';
-    }
-    ?>    
+   <h2>Banner comming soon</h2>    
 </div>
