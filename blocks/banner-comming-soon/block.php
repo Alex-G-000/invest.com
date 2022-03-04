3900c04c9502dcfc870746523e05e4ca9e4493f9
Alexey-Geyvan <alexey.geyvan@kawalee.com>
2022-03-04 10:18:02 +0200

    Update block.php

--- blocks/banner-comming-soon/block.php
+++ blocks/banner-comming-soon/block.php
@@ -11,44 +11,44 @@
         }    
     
         if(!empty(block_value('background'))){
-            $banner_bg = block_value('background');
+            $banner_soon_bg = block_value('background');
         } else {
-            $banner_bg = wp_get_attachment_image_url( $args['banner-background'], 'full' );
+            $banner_soon_bg = $args['banner-background'];
         }
     
         if(!empty(block_value('image'))){
-            $banner_img = block_value('image');
+            $banner_soon_img = block_value('image');
         } else {
-            $banner_img = wp_get_attachment_image_url( $args['banner-image'], 'medium' );
+            $banner_soon_img = $args['banner-image'];
         }
 
         if(!empty(block_value('link'))){
-            $banner_link = block_value('link');
+            $banner_soon_link = block_value('link');
         } else {
-            $banner_link = $args['link'];
+            $banner_soon_link = $args['link'];
         }
 
-        if(!empty($banner_link)) {
-            $banner_onclick = ' onclick="';
-            $banner_onclick.= "window.location.href='";
-            $banner_onclick.= $banner_link;
-            $banner_onclick.= "';";
-            $banner_onclick.= '"';
+        if(!empty($banner_soon_link)) {
+            $banner_soon_onclick = ' onclick="';
+            $banner_soon_onclick.= "window.location.href='";
+            $banner_soon_onclick.= $banner_soon_link;
+            $banner_soon_onclick.= "';";
+            $banner_soon_onclick.= '"';
         } else {
-            $banner_onclick = "";
+            $banner_soon_onclick = "";
         }
      
  ?>
  
  <section class="banner-comming-soon block<?php block_field('additional-class'); ?>">
      <div class="container">
-         <div class="block__inner rounded" style="background-image:url('<?php echo $banner_bg; ?>')" <?php echo $banner_onclick; ?> >
+         <div class="block__inner rounded" style="background-image:url('<?php echo $banner_soon_bg; ?>')" <?php echo $banner_soon_onclick; ?> >
              <div class="row">
                  <div class="block__content col-12 col-md-6 col-lg-7 text-center text-md-left">
                      <h2 class="block__title text-white"><?php echo $title; ?></h2>                
                  </div>
                  <div class="col-6 col-lg-5 d-none d-md-flex justify-content-center">
-                     <img class="block__image" src="<?php echo $banner_img; ?>" alt="" >
+                     <img class="block__image" src="<?php echo $banner_soon_img; ?>" alt="" >
                  </div>
              </div>
          </div>
