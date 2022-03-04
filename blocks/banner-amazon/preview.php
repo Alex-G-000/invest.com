c12b8459b47e362babf6e41d4dd1b8a8c79554b7
Alexey-Geyvan <alexey.geyvan@kawalee.com>
2022-03-04 10:34:57 +0200

    Update preview.php

--- blocks/banner-amazon/preview.php
+++ blocks/banner-amazon/preview.php
@@ -1,9 +1,3 @@
 <div class="block-preview">
-    <?php if (empty(block_value('preview-image'))) { ?>
-       <h3>Section</h3>
-    <?php } else { ?>
-      <img src="<?php block_field('preview-image'); ?>" style="width:100%; height:auto;">
-    <?php } ?> 
-    <?php $template = str_replace(array("invest\\", "\\", ".php"), array("", "/", ""), block_value('template')); ?>
-    <div class="template-path"><p><?php echo $template; ?></p></div>
+   <h2>Banner Amazon</h2>    
 </div>
