1c132381bdfd7ff5d4c0582ac4f25204bea08b37
Alexey-Geyvan <alexey.geyvan@kawalee.com>
2022-03-18 15:55:44 +0200

    Comming soon LP

--- block-templates/landing-pages/stock-investing/comming-soon/template.php
+++ block-templates/landing-pages/stock-investing/comming-soon/template.php
@@ -14,14 +14,18 @@
 
 
 
-<section class="stock-comming-soon block">    
+<section class="stock-comming-soon block mb-0 d-flex justify-content-center align-items-center<?php echo $some_class; ?>">    
     <div class="container">
         <div class="row">
-
-
+            <div class="col-12 d-flex flex-column justify-content-center align-items-center text-white text-center">
+                <h5 class="block__small-text mb-4">Comming soon</h5>                
+                <h1 class="block__title mb-4" data-aos="fade-up"><?php echo $title; ?></h2>
+                <p class="block__subtitle mb-5"><?php echo $subtitle; ?></p>   
+            </div>
+            <div class="col-12 text-center d-flex justify-content-center">
+                <a class="btn btn-secondary mr-3" href="<?php echo home_url(); ?>" >Home Page</a>
+                <button class="btn btn-outline-secondary" onclick="history.back()" >Back</a>
+            </div>
         </div>
     </div>
 </section>
-
-
-
